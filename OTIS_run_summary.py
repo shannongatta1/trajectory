313440d5fa3e9451ce83d2dff8d38683c14f4dca
Bum Mook Oh <bmo5@uw.edu>
2018-04-04 19:33:18 -0700

    implicit and explicit reading

--- OTIS_run_summary.py
+++ OTIS_run_summary.py
@@ -6,14 +6,16 @@ Version 1.0 - Bryan Munro - 7/25/2017
 """
 import sys
 
+
 def ReadOP1File():
     import sys
     import os
     import glob
     from pathlib import Path
-    #Initialize variables
+    # Initialize variables
     # inputdir = sys.argv[1]
-    inputdir = "/Users/bummookoh/Projects/trajectory/example" #'S:\\4-ENGINEERING\\18-Software\\OTIS_RunSummary\\Smaller sample'
+    # 'S:\\4-ENGINEERING\\18-Software\\OTIS_RunSummary\\Smaller sample'
+    inputdir = "/Users/bummookoh/Projects/trajectory/example"
     # inputdir = 'S:\\4-ENGINEERING\\18-Software\\OTIS_RunSummary\\foldertest'
     EmptyLineCounter = 0
     HeaderLine = False
@@ -21,7 +23,6 @@ def ReadOP1File():
     ExplicitLine = False
     PreviousLine = False
     index = 0
-
 
     full_path = []
     dirList = glob.glob(inputdir + "/*/*.op1")
@@ -31,73 +32,107 @@ def ReadOP1File():
     print full_path
 
     for filePath in full_path:
-        get_first_and_last_line(filePath)
+        heading, first_para, second_para, isTwoPara = get_first_and_last_line(
+            filePath)
+        print heading
+        print first_para[0]
+        print first_para[1]
 
-                                                                    
+        print second_para[0]
+        print second_para[1]
+
+
 def get_first_and_last_line(file_path):
+    start_first_paragraph = False
+    start_second_paragraph = False
+    first_para = {}
+    second_para = {}
     with open(file_path, "rb") as f:
         first_line = f.readline()
-        for last_line in f: pass 
-    print first_line
-    print last_line
-            
 
-        #Read the file line by line
-        # with input as OP1File:
+        for line in f:
+            if line.strip() == "" and not start_first_paragraph:
+                start_first_paragraph = True
+                continue
+            elif line.strip() == "" and not start_second_paragraph:
+                start_first_paragraph = False
+                start_second_paragraph = True
+                continue
 
-        #         #Loop through file
-        #         for LineIndex, CurrentLine in enumerate(OP1File):
+            if len(first_para) < 1:
+                first_para[0] = line
+            elif start_first_paragraph:
+                first_para[1] = line
 
-        #             #Grab the first line and store as the header line
-        #             if LineIndex == 0:
-        #                 HeaderLine = CurrentLine
-        #                 #print "Header Line: " + CurrentLine
+            if len(second_para) < 1:
+                second_para[0] = line
+                
+            elif start_second_paragraph:
+                second_para[1] = line
 
-        #             #Check for empty lines and increment counter
-        #             if not CurrentLine.strip():
-        #                 EmptyLineCounter += 1
-        #                 #print "Hit Empty Line" + CurrentLine
+    return (first_line,first_para,second_para,start_second_paragraph)
 
-        #             #If this is the second empty line, then we have encountered the end of the implicit data block
-        #             if (EmptyLineCounter == 2 and not ImplicitLine):
-        #                 ImplicitLine = PreviousLine
-        #                 #print "Hit Implicit Line: " + ImplicitLine
 
-        #             #If this is the third empty line, then we have encountered the end of the explicit data block
-        #             if (EmptyLineCounter == 3 and not ExplicitLine):
-        #                 ExplicitLine = PreviousLine
-        #                 #print "Hit Explicit Line: " + ExplicitLine
+def convertToCsv(text):
+    return ",".join(text.split())
 
-        #             #Set the previous line
-        #             PreviousLine = CurrentLine
+    # Read the file line by line
+    # with input as OP1File:
 
-        #         #Otherwise, the end of the file is reached
-        #         else:
-        #             #print "Hit the end of the file: " + PreviousLine
-        #             ExplicitLine = PreviousLine
+    #         #Loop through file
+    #         for LineIndex, CurrentLine in enumerate(OP1File):
 
-        #     #If an explicit line but not an implicit line was found, duplicate
-        #     if (not ImplicitLine) and (ExplicitLine):
-        #         #print "Over writing implicit with explicit: " + ExplicitLine
-        #         ImplicitLine = ExplicitLine
-            
-        #     with ExplicitLine as el:
-        #     first = next(el).decode()
+    #             #Grab the first line and store as the header line
+    #             if LineIndex == 0:
+    #                 HeaderLine = CurrentLine
+    #                 #print "Header Line: " + CurrentLine
 
-        #     fh.seek(-1024, 2)
-        #     last = el.readlines()[-1].decode()
+    #             #Check for empty lines and increment counter
+    #             if not CurrentLine.strip():
+    #                 EmptyLineCounter += 1
+    #                 #print "Hit Empty Line" + CurrentLine
 
-        #     with ImplicitLine as il:
-        #     first = next(il).decode()
+    #             #If this is the second empty line, then we have encountered the end of the implicit data block
+    #             if (EmptyLineCounter == 2 and not ImplicitLine):
+    #                 ImplicitLine = PreviousLine
+    #                 #print "Hit Implicit Line: " + ImplicitLine
 
-        #     fh.seek(-1024, 2)
-        #     last = il.readlines()[-1].decode()
+    #             #If this is the third empty line, then we have encountered the end of the explicit data block
+    #             if (EmptyLineCounter == 3 and not ExplicitLine):
+    #                 ExplicitLine = PreviousLine
+    #                 #print "Hit Explicit Line: " + ExplicitLine
 
-        #     #Split the return lines using spaces (the default) and return the associated lists
-        #     HeaderList = HeaderLine.split()
-        #     ImplicitList = ImplicitLine.split()
-        #     ExplicitList = ExplicitLine.split()
+    #             #Set the previous line
+    #             PreviousLine = CurrentLine
 
-        #     #Return the data
-        #     return HeaderList, ImplicitList, ExplicitList;
+    #         #Otherwise, the end of the file is reached
+    #         else:
+    #             #print "Hit the end of the file: " + PreviousLine
+    #             ExplicitLine = PreviousLine
+
+    #     #If an explicit line but not an implicit line was found, duplicate
+    #     if (not ImplicitLine) and (ExplicitLine):
+    #         #print "Over writing implicit with explicit: " + ExplicitLine
+    #         ImplicitLine = ExplicitLine
+
+    #     with ExplicitLine as el:
+    #     first = next(el).decode()
+
+    #     fh.seek(-1024, 2)
+    #     last = el.readlines()[-1].decode()
+
+    #     with ImplicitLine as il:
+    #     first = next(il).decode()
+
+    #     fh.seek(-1024, 2)
+    #     last = il.readlines()[-1].decode()
+
+    #     #Split the return lines using spaces (the default) and return the associated lists
+    #     HeaderList = HeaderLine.split()
+    #     ImplicitList = ImplicitLine.split()
+    #     ExplicitList = ExplicitLine.split()
+
+
+    #     #Return the data
+    #     return HeaderList, ImplicitList, ExplicitList;
 ReadOP1File()
